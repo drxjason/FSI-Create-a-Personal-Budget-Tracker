e1860cad7e5d388437f59ad00695f330860ac074
Jason Diaz <jason@Jasons-MacBook-Air.local>
2022-01-24 22:00:10 -0700

    add js

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ let monthlyExpenses = 0
 let annualExpenses = 0
 
 // Your Code Here
+
+var answer = null
+
+for (let i = 0; i < weeklyExpenseQuestions.length; i++) {
+	answer = window.prompt(weeklyExpenseQuestions[i])
+	answer = parseFloat(answer)
+	weeklyExpenses += answer
+}
